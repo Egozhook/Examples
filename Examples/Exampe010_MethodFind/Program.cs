3b8881cf0bf3374a2ae76e08fd1a2ccadebef5dd
egozhook <egozhook@gmail.com>
2022-12-06 13:07:34 +0400

    Find

---
+++ Examples/Exampe010_MethodFind/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {12, 65, 18, 32, 72, 98, 47, 5};
+int n = array.Length;
+int find = 47;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
